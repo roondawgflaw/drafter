662868aa323f041fc159325ce7fe73527364f563
Aidan <aidan@example.com>
2026-01-02 10:12:39 -0500

    chore: remove invalid prisma.config.ts (not needed, causes build error)

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // Migrate uses this URL. Set DATABASE_URL in .env
-    url: process.env.DATABASE_URL!,
-  },
-});

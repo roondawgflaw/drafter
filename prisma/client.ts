744f608a85b7dc650e8ee32e75a7adfd2064a987
Aidan <aidan@example.com>
2026-01-02 11:51:35 -0500

    chore: update NEXTAUTH_URL for production deployment

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,11 +1,18 @@
+
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool } from '@neondatabase/serverless';
+
+const connectionString = process.env.DATABASE_URL!;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ['error'],
   });
 
